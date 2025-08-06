0d49218e6456deda653dc0b54177f7d824c31ffe
moathe <moathe2012@hotmail.com>
2025-08-06 16:46:59 +0300

    11

--- Ultimate.IntegrationSystem.Web/Program.cs
+++ Ultimate.IntegrationSystem.Web/Program.cs
@@ -1,19 +1,26 @@
-
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 using MudBlazor.Services;
 using Ultimate.IntegrationSystem.Web.Service;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
 builder.Services.AddSingleton<EmployeeService>();
-
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources/Localization");
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] { new CultureInfo("en-US"), new CultureInfo("ar-YE") };
+    options.DefaultRequestCulture = new RequestCulture("ar-YE");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
 var app = builder.Build();
-
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 app.UseStaticFiles();
 app.UseRouting();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
