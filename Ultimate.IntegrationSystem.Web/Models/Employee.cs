45fb8d8aaf47835718f1ae7167817a734e00e476
moathe <moathe2012@hotmail.com>
2025-08-07 23:29:41 +0300

    ok

--- Ultimate.IntegrationSystem.Web/Models/Employee.cs
+++ Ultimate.IntegrationSystem.Web/Models/Employee.cs
@@ -2,12 +2,15 @@
 {
     public class Employee
     {
+       
+
+
         public int EmployeeNumber { get; set; }
-        public string Name { get; set; }
-        public string JobTitle { get; set; }
-        public string Department { get; set; }
-        public string Branch { get; set; }
+        public string Name { get; set; } = "";
+        public string JobTitle { get; set; } = "";
+        public string Department { get; set; } = "";
+        public string Branch { get; set; } = "";
         public DateTime JoinDate { get; set; }
-        public string Status { get; set; }
+        public string Status { get; set; } = "Active"; // Active / Inactive
     }
 }
