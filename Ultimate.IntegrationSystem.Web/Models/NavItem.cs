2b633f467c6da284b715069a09ec48650190e3bd
moathe <moathe2012@hotmail.com>
2025-08-07 19:58:13 +0300

    ff

---
+++ Ultimate.IntegrationSystem.Web/Models/NavItem.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using MudBlazor;
+
+namespace Ultimate.IntegrationSystem.Web.Models
+{
+    public class NavItem
+    {
+        public string TitleKey { get; set; } = "";
+        public string Icon { get; set; } = Icons.Material.Filled.Home;
+        public string? Href { get; set; }
+        public NavLinkMatch Match { get; set; } = NavLinkMatch.All;
+        public string[]? Roles { get; set; }
+        public List<NavItem>? Children { get; set; }
+    }
+}
