c6bc1237d47e95b167eaae4c7597d7e788190c23
moathe <moathe2012@hotmail.com>
2025-08-10 11:36:18 +0300

    شيي

--- Ultimate.IntegrationSystem.Web/Program.cs
+++ Ultimate.IntegrationSystem.Web/Program.cs
@@ -9,31 +9,28 @@ using Ultimate.IntegrationSystem.Web.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Razor Pages + Blazor Server
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+// „Â„ ·„·›«  _content/* Ê wwwroot/*
+builder.WebHost.UseStaticWebAssets();
 
-// MudBlazor
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor()
+    .AddCircuitOptions(o => o.DetailedErrors = true);
+
 builder.Services.AddMudServices();
 
-// Œœ„« ﬂ
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
-// Localization
 builder.Services.AddLocalization(o => o.ResourcesPath = "Resources/Localization");
 builder.Services.Configure<RequestLocalizationOptions>(o =>
 {
     var cultures = new[] { new CultureInfo("ar-YE"), new CultureInfo("en-US") };
-    o.DefaultRequestCulture = new RequestCulture("ar-YE");
+    o.DefaultRequestCulture = new("ar-YE");
     o.SupportedCultures = cultures;
     o.SupportedUICultures = cultures;
 });
-builder.Services.AddServerSideBlazor()
-    .AddCircuitOptions(o => o.DetailedErrors = true);
 
 var app = builder.Build();
 
-// Middleware
 app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 
 if (!app.Environment.IsDevelopment())
@@ -42,7 +39,6 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseRouting();
 
-// Endpoints
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
